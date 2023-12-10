79d33e6e83b7623246cc2ea7985d1edbc6de3e6d
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-10 16:26:42 +0100

    Update Esp8266-Dht11.ino

--- Esp8266-Dht11.ino
+++ Esp8266-Dht11.ino
@@ -1,33 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <DHT.h>
 
-const char *ssid = "TvojeSSID";
+const char *ssid = "EspWiFi";
 const char *password = "TvojeHeslo";
-
-const char *serverIP = "IP_adresa_druheho_ESP8266";
-const int serverPort = 80;
 
 DHT dht(D2, DHT11);
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
+  WiFi.softAP(ssid, password);
 }
 
 void loop() {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
 
-  // Odoslanie údajov na druhý ESP8266
-  sendToServer(temperature, humidity);
+  // Implementuj kód pre odoslanie údajov na druhý ESP8266 cez UDP alebo TCP
 
   delay(6000);  // Pauza 6 sekúnd
-}
-
-void sendToServer(float temp, float hum) {
-  // Implementuj kód pre odosielanie údajov na druhý ESP8266 cez HTTP POST request
 }
